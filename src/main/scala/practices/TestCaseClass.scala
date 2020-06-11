360d9a76d36d161f81b85d8475209176b56960b3
avichalkesharwani <kesharwani.avichal@gmail.com>
2020-06-11 16:37:27 +0530

    test class, test case class with switch case

---
+++ src/main/scala/practices/TestCaseClass.scala
@@ -0,0 +1,28 @@
+package practices
+
+case class TestCaseClass(name: String, age: Int)
+// USed as data transfer object
+
+
+object TestCaseClassDriver {
+  def main(args: Array[String]) {
+    // apply methods generated
+    val p1 = TestCaseClass("Avichal", 40)
+    val p2 = new TestCaseClass("Ravi", 32)
+    // equals method generated
+    println(p1 == p2)
+    // copy method generated
+    val p3 = p1.copy(name = "Ganesh")
+    println(p3.toString())
+
+    // Writing switch case inside the println
+    println {
+      p2.name match {
+        case "Avichal" => "Match found..."
+        case _ => "No suitable match found..."
+      }
+
+
+    }
+  }
+}
