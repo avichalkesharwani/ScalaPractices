8df876bde2e38e2736704ad8a197170f6768bf34
avichalkesharwani <kesharwani.avichal@gmail.com>
2020-06-15 13:47:29 +0530

    more

--- src/main/scala/catsexercise/TypeSuperAdder1.scala
+++ src/main/scala/catsexercise/TypeSuperAdder1.scala
@@ -12,14 +12,33 @@ object TypeSuperAdder1 extends App {
 
   // def add(items: List[Int]): Int = items.foldLeft(0)(_ + _)
 
-   // another way
+   // another way ...
 
   import cats.Monoid
   import cats.instances.int._    // for Monoid
   import cats.syntax.semigroup._ // for |+|
+
+  /*
   def add(items: List[Int]): Int =
   items.foldLeft(Monoid[Int].empty)(_ |+| _)
+  */
 
-  
+
+  /* Well done! SuperAdder’s market share con􏰀nues to grow, and now there is demand for addi􏰀onal func􏰀onality.
+  People now want to add List[Option[Int]]. Change add so this is possible. The SuperAdder code base is of the highest
+  quality, so make sure there is no code duplica􏰀on!
+  */
+
+  def add[A](items: List[A])(implicit monoid: Monoid[A]): A =
+    items.foldLeft(monoid.empty)(_ |+| _)
+
+  import cats.instances.int._ // for Monoid
+
+  println(add(List(1, 2, 3)))
+  // res9: Int = 6
+  import cats.instances.option._ // for Monoid
+  println(add(List(Some(1), None, Some(2), None, Some(3))))
+
+
 
 }
